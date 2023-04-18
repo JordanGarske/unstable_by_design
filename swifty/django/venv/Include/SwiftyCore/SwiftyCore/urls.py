e0ca5bd7f9f720b93fdfd24cd201e75653cfd45e
jordan <jordago9980@example.com>
2023-04-18 16:02:56 -0500

    django frame
    f

--- swifty/django/venv/Include/SwiftyCore/SwiftyCore/urls.py
+++ swifty/django/venv/Include/SwiftyCore/SwiftyCore/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework import serializers
 
 urlpatterns = [
     path('admin/', admin.site.urls),
