1f03680c0107ad5798a43a4b643fca65deaa613e
JeanAEckelberg <fyrelynx11@gmail.com>
2023-04-18 17:16:15 -0500

    Create Serializers
    Initial push

--- swifty/django/venv/Include/SwiftyCore/SwiftyCore/settings.py
+++ swifty/django/venv/Include/SwiftyCore/SwiftyCore/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
     'projectmanagement'
 ]
 
@@ -48,6 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+     'http://localhost:3000'
 ]
 
 ROOT_URLCONF = 'SwiftyCore.urls'
---
+++ swifty/django/venv/Include/SwiftyCore/projectmanagement/serializers.py
@@ -0,0 +1,47 @@
+from rest_framework import serializers
+from .models import User, Project, Role, RoleToChannel, Channel, Message, Status, Task
+
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ['ProjectID', 'Name', 'Description', 'Color']
+    
+class RoleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Role
+        fields = ['RoleID', 'Name', 'Description', 'Color', 'ProjectID', 'Channels']
+        extra_kwargs = {'Users': {'required': False}}
+
+class UserSerializer(serializers.ModelSerializer):
+    Roles = RoleSerializer(many=True, read_only=True)
+    class Meta:
+        model = User
+        fields = ['UserID', 'Username', 'Password']
+        extra_kwargs = {'Roles': {'required': False}}
+
+class RoleToChannelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RoleToChannel
+        fields = []
+
+class ChannelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Channel
+        fields = []
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = ['MessageID', 'AuthorID', 'ChannelID', 'MessageContent']
+
+class StatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Status
+        fields = ['StatusID', 'Name', 'Description', 'ProjectID']
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ['TaskID', 'Name', 'Description', 'StatusID', 'AuthorID']
