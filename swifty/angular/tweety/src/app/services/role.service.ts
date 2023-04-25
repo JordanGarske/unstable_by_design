44bbbe503fd6c4966ae61f1717ea0905e3ba952e
jordan <jordago9980@example.com>
2023-04-25 17:56:54 -0500

    role

--- swifty/angular/tweety/src/app/services/role.service.ts
+++ swifty/angular/tweety/src/app/services/role.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Task } from '../actors/task';
+import { Role } from '../actors/role';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoleService {
-
-  constructor() { }
+  djangoURL = 'localhost:8000/api'
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+  constructor(private http: HttpClient) { }
+  getTasks(id:number) :Observable<Role>{
+    return this.http.get<Role>(this.setUpTasks(id)).pipe();
+  }
+  setUpTasks(id:number): string{
+    return `${this.djangoURL}/Roles/${id }`;
+  }
+  deleteUser(id:number): Observable<Role>{
+    return this.http.get<Role>(this.setUpTasks(id)).pipe();
+  }
+  addUser(hero: Role): Observable<Role> {
+    return this.http.post<Role>(this.djangoURL, hero, this.httpOptions).pipe(
+    );
+  }
 }
