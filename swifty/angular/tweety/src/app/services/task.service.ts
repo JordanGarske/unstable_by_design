47c6c8eaba03bc89d81b0d8e2ea314a7490413b0
jordan <jordago9980@example.com>
2023-04-25 17:53:36 -0500

    task

--- swifty/angular/tweety/src/app/services/task.service.ts
+++ swifty/angular/tweety/src/app/services/task.service.ts
@@ -1,10 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Task } from '../actors/task';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-
-  constructor() { }
-  
+  djangoURL = 'localhost:8000/api'
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+  constructor(private http: HttpClient) { }
+  getTasks(id:number) :Observable<Task>{
+    return this.http.get<Task>(this.setUpTasks(id)).pipe();
+  }
+  setUpTasks(id:number): string{
+    return `${this.djangoURL}/Tasks/${id }`;
+  }
+  deleteUser(id:number): Observable<Task>{
+    return this.http.get<Task>(this.setUpTasks(id)).pipe();
+  }
+  addUser(hero: Task): Observable<Task> {
+    return this.http.post<Task>(this.djangoURL, hero, this.httpOptions).pipe(
+    );
+  }
 }
+
