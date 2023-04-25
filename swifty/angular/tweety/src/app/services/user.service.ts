7be5170d61a76d6bc6ee6a8051a390dd8782f57c
jordan <jordago9980@example.com>
2023-04-25 17:51:05 -0500

    user

--- swifty/angular/tweety/src/app/services/user.service.ts
+++ swifty/angular/tweety/src/app/services/user.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../actors/user';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-
-  constructor() { }
+  djangoURL = 'localhost:8000/api'
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+  constructor(private http: HttpClient) { }
+  getUsers(id:number) :Observable<User>{
+    return this.http.get<User>(this.setUpUsers(id)).pipe();
+  }
+  setUpUsers(id:number): string{
+    return `${this.djangoURL}/Users/${id }`;
+  }
+  deleteUser(id:number): Observable<User>{
+    return this.http.get<User>(this.setUpUsers(id)).pipe();
+  }
+  addUser(hero: User): Observable<User> {
+    return this.http.post<User>(this.djangoURL, hero, this.httpOptions).pipe(
+    );
+  }
 }
