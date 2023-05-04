52c460d2276a6e6aacb442eb90991a5d45cc82bd
JeanAEckelberg <fyrelynx11@gmail.com>
2023-05-03 20:01:18 -0500

    Update

--- swifty/angular/tweety/src/app/home/task-edit/task-edit.component.ts
+++ swifty/angular/tweety/src/app/home/task-edit/task-edit.component.ts
@@ -3,6 +3,12 @@ import { Status } from 'src/app/actors/status';
 import { Task } from 'src/app/actors/task';
 import { CurrentUserStorageService } from 'src/app/current-user-storage.service';
 import { TaskService } from 'src/app/services/task.service';
+import { Project } from 'src/app/actors/project';
+import { ProjectService } from 'src/app/services/project.service';
+import { StatusService } from 'src/app/services/status.service';
+import { PrivacyLevel } from 'discord.js';
+import { forkJoin } from 'rxjs';
+
 
 @Component({
   selector: 'app-task-edit',
@@ -11,17 +17,27 @@ import { TaskService } from 'src/app/services/task.service';
 })
 export class TaskEditComponent implements OnInit {
   task:Task = {} as Task;
+  project: Project = {} as Project;
   statusOptions: Status[] = [];
-  constructor(private taskService: TaskService, private userStroage: CurrentUserStorageService){}
+  statuses: Status[] = [];
+  constructor(private taskService: TaskService, private statusService: StatusService,private userStroage: CurrentUserStorageService){}
   ngOnInit( ): void {
     this.userStroage.getCurrentTask$().subscribe(selected => {
       
       if(selected){
         console.log(selected.AuthorID);
       this.task = selected;
-      this.statusOptions = this.userStroage.getProjectStatusID().filter( x => x !=undefined);
+      this.userStroage.getProjectStatusID().filter( x => x !=undefined);
       this.userStroage.setCurrentTask$(undefined);
       }}).unsubscribe();
+    this.userStroage.getCurrentProject$().subscribe(x => {if(x) this.project = x})
+    console.log(this.project)
+    this.statusService.getStatuses().subscribe(x => {
+      this.statusOptions = x;
+      this.statusOptions = this.statusOptions.filter(x => x.ProjectID === this.project.ProjectID)
+    })
+    console.log(this.statuses)
+    
   }
   updateTask():void{
     this.taskService.updateTask(this.task);
