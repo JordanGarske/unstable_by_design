435b3e93648ad4945c8ce6eafc75575034b81e11
MrMuffin56 <shannonderek56@gmail.com>
2023-05-04 16:30:18 -0500

    Statuses Now show properly

--- swifty/angular/tweety/src/app/home/project-overview/project-overview.component.ts
+++ swifty/angular/tweety/src/app/home/project-overview/project-overview.component.ts
@@ -10,15 +10,21 @@ import { ProjectService } from 'src/app/services/project.service';
 })
 export class ProjectOverviewComponent implements OnInit {
   project = {} as Project;
-  constructor(private projectService: ProjectService, private userStorage: CurrentUserStorageService){}
-  ngOnInit( ): void {
-    this.userStorage.getCurrentProject$().subscribe(selected => {
-      if(selected){
-        this.project = selected;
-        this.userStorage.setCurrentProject$(undefined);
-      }}).unsubscribe();
+  constructor(
+    private projectService: ProjectService,
+    private userStorage: CurrentUserStorageService
+  ) {}
+  ngOnInit(): void {
+    this.userStorage
+      .getCurrentProject$()
+      .subscribe((selected) => {
+        if (selected) {
+          this.project = selected;
+        }
+      })
+      .unsubscribe();
   }
-  updateProject():void{
+  updateProject(): void {
     this.projectService.updateProject(this.project).subscribe();
   }
-} 
+}
