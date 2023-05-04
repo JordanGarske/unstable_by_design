7cac20eeeeea700a6719fddee95db519cffe3ddf
jordan <jordago9980@example.com>
2023-05-03 22:27:02 -0500

    project create is up needs temp role and status

--- swifty/angular/tweety/src/app/home/project-create/project-create.component.ts
+++ swifty/angular/tweety/src/app/home/project-create/project-create.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Project } from 'src/app/actors/project';
+import { CurrentUserStorageService } from 'src/app/current-user-storage.service';
 import { ProjectService } from 'src/app/services/project.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-project-create',
@@ -8,10 +10,12 @@ import { ProjectService } from 'src/app/services/project.service';
   styleUrls: ['./project-create.component.scss']
 })
 export class ProjectCreateComponent {
-  project: Project = {} as Project;
-  constructor(private projectService: ProjectService) {}
+  newProject: Project = {} as Project;
+  constructor(private userStorage: CurrentUserStorageService  ,private projectService: ProjectService ,private userService: UserService) {}
 
   createProject():void{
-    this.projectService.addProject(this.project);
+    this.projectService.addProject(this.newProject).subscribe(value =>{
+        console.log(value);
+    });
   } 
 }
