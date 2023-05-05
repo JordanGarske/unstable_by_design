1b3b778278393655dc7b8b5cae1d3861d0af9ee5
JeanAEckelberg <fyrelynx11@gmail.com>
2023-05-04 19:41:09 -0500

    UPdate

--- swifty/angular/tweety/src/app/home/task-edit/task-edit.component.ts
+++ swifty/angular/tweety/src/app/home/task-edit/task-edit.component.ts
@@ -5,7 +5,7 @@ import { CurrentUserStorageService } from 'src/app/current-user-storage.service'
 import { TaskService } from 'src/app/services/task.service';
 import { Project } from 'src/app/actors/project';
 import { StatusService } from 'src/app/services/status.service';
-import { forkJoin } from 'rxjs';
+import { first, forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-task-edit',
@@ -13,29 +13,34 @@ import { forkJoin } from 'rxjs';
   styleUrls: ['./task-edit.component.scss']
 })
 export class TaskEditComponent implements OnInit {
+  selectedStatus: Status = {} as Status;
   task:Task = {} as Task;
   project: Project = {} as Project;
   statusOptions: Status[] = [];
   statuses: Status[] = [];
   constructor(private taskService: TaskService, private statusService: StatusService,private userStroage: CurrentUserStorageService){}
   ngOnInit( ): void {
-    this.userStroage.getCurrentTask$().subscribe(selected => {
+    this.userStroage.getCurrentTask$().pipe(first()).subscribe(selected => {
       
       if(selected){
-        console.log(selected.AuthorID);
-      this.task = selected;
-      this.userStroage.getProjectStatusID().filter( x => x !=undefined);
-      this.userStroage.setCurrentTask$(undefined);
-      }}).unsubscribe();
+        this.task = selected;
+        this.userStroage.getProjectStatusID().filter( x => x !=undefined);
+        this.userStroage.setCurrentTask$(undefined);
+      }});
 
 
-    this.userStroage.getCurrentProject$().subscribe(x => {if(x) this.project = x})
-    forkJoin(this.project.Statuses.map(x => this.statusService.getStatusById(x))).subscribe(x => this.statusOptions = x)
+    this.userStroage.getCurrentProject$().pipe(first()).subscribe(x => {if(x) this.project = x})
+    forkJoin(this.project.Statuses.map(x => this.statusService.getStatusById(x))).pipe(first()).subscribe(x => this.statusOptions = x)
     
   }
+
+  updateStatusID(e: any){
+    this.task.StatusID = Number.parseInt(e.target.value)
+  }
+
   updateTask():void{
     console.log(this.task)
-    this.taskService.updateTask(this.task).subscribe();
+    this.taskService.updateTask(this.task).pipe(first()).subscribe();
   }
   
 }
