de066e62a9d949bb7d60738dad21007fabe14e63
jordan <jordago9980@example.com>
2023-05-05 11:08:23 -0500

    project overview

--- swifty/angular/tweety/src/app/home/member-overview/member-overview.component.ts
+++ swifty/angular/tweety/src/app/home/member-overview/member-overview.component.ts
@@ -11,22 +11,35 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class MemberOverviewComponent  implements OnInit{
   id?:number;
-  allUsers: User[]= [];
+  projectEmployee: User[]= [];
+  hires: User[] = [];
   constructor(private userService: UserService, private roleService: RoleService, private userStorage: CurrentUserStorageService ){ }
   ngOnInit():void{
     this.userStorage.getCurrentProject$().subscribe(proj =>{
        this.roleService.getRoles().subscribe(roles=> this.id = roles.filter( role => role.ProjectID === proj?.ProjectID
          && role.Name === "employee" )[0].RoleID )}) ;
-    this.userService.getUsers().subscribe(users => this.allUsers = users);
+    this.userService.getUsers().subscribe(users => this.dividUser(users));
     
   }
+  dividUser(users: User[]):void{
+   this.projectEmployee= [];
+    this.hires = [];
+      users.forEach(user => {
+        if(this.id){
+
+          if(!user.Roles.includes(this.id)){ this.projectEmployee.push(user)}
+          else{ this.hires.push(user)}
+
+      }})
+    }
+  
   selectedUser(user: User):void {
     if(this.id){
      if(!user.Tasks){
         user.Tasks = [];
      }
        user.Tasks.push(this.id); 
-      this.userService.addUser(user).subscribe(x => console.log(x));
+      this.userService.updateUser(user).subscribe(x => this.userService.getUsers().subscribe(users => this.dividUser(users) ));
     }
   }
    
